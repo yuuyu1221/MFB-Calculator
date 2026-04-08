44855206fded0de452211714c54b7fbfda4b7ce9
yuuyu1221 <firemonster1221@gmail.com>
2026-04-08 15:13:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -241,7 +241,8 @@ function formatCompact(num) {
   if (num >= 1e12) return (num / 1e12).toFixed(2).replace(/\.00$/, '') + 'T';
   if (num >= 1e9)  return (num / 1e9).toFixed(2).replace(/\.00$/, '') + 'B';
   if (num >= 1e6)  return (num / 1e6).toFixed(2).replace(/\.00$/, '') + 'M';
-
+  if (num >= 1e3)  return (num / 1e3).toFixed(2).replace(/\.00$/, '') + 'K';
+    
   return num.toLocaleString();
 }
 function formatCurrencyCompact(num) {
